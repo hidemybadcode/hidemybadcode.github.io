d6d0a5d95bbbf98c4e5c545f565b04b585a13703
hidemybadcode <105995960+hidemybadcode@users.noreply.github.com>
2022-05-30 11:07:26 -0400

    Update customSocialLinks.js

--- assets/js/insertParts/customSocialLinks.js
+++ assets/js/insertParts/customSocialLinks.js
@@ -1,15 +1,15 @@
 const linkInsert = `
   <li>
-    <a href="https://www.linkedin.com/in/madhav-malhotra/" target="_blank"><i class="fa fa-linkedin"></i></a>
+    <a href="https://www.linkedin.com/in/tobiasgrethermurray/" target="_blank"><i class="fa fa-linkedin"></i></a>
   </li>
   <li>
-    <a href="https://madhav-malhotra003.medium.com/" target="_blank"><i class="fa fa-medium"></i></a>
+    <a href="https://medium.com/@tobias_gm" target="_blank"><i class="fa fa-medium"></i></a>
   </li>
   <li>
-    <a href="https://github.com/Madhav-Malhotra" target="_blank"><i class="fa fa-github"></i></a>
+    <a href="https://twitter.com/grethermurrayt" target="_blank"><i class="fa fa-twitter"></i></a>
   </li>
   <li>
-    <a href="https://www.youtube.com/channel/UCWopBNISjL7qCH2Qch58Skw" target="_blank"><i class="fa fa-youtube-play"></i></a>
+    <a href="https://www.youtube.com/channel/UC1TMSm8CAmFTZH2EeU87Lfg" target="_blank"><i class="fa fa-youtube-play"></i></a>
   </li>
 `;
 
